37d2b6fc58d5debd154b67a8aebfb45632733f11
Tris860 <sezeranoshimo@gmail.com>
2025-11-23 12:31:01 +0200

    ATS_CLIENT

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 # --- Configuration ---
 WS_HOST = "0.0.0.0"
 WS_PORT = int(os.environ.get("PORT", 8765))  # Render sets this automatically
-SERVER_B_URL = os.environ.get("SERVER_B_URL", "http://localhost:5000/command")
+SERVER_B_URL = os.environ.get("SERVER_B_URL", "https://iot-gateway-server.onrender.com")
 
 CONNECTED_CLIENTS = set()
 
